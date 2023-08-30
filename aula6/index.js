0bf64378e523e8e0821ef1cc8d676d59696c6f0f
lelo <aurelioarruda@live.com>
2023-08-30 15:03:07 -0300

    hjhj

---
+++ aula6/index.js
@@ -0,0 +1,6 @@
+let nome = 'aurelio' ; //string
+
+console.log(nome, 'nasceu em 1991.');
+console.log('em 2000', nome, 'conheceu Gabi.');
+console.log('Gabi teve 1 filho com',nome, 'em 2015.');
+console.log('o filho de', nome, 'se chama Eduardo.');
